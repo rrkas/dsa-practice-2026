8ff2c8566951f264bfea5b02668c0e0f6faa2532
Rohnak Agarwal <rrka79wal@gmail.com>
2026-03-20 20:09:03 +0530

    hacker-rank levels added

--- _platform_score_level.py
+++ _platform_score_level.py
@@ -1,23 +1,34 @@
 def get_platform_score_level(platform: str, prob_name: str):
     prob_name_parts = prob_name.split("--")
-    if len(prob_name_parts) == 1:
-        raise NameError(
-            f"{platform}/{prob_name} has no score or difficulty level in name (--)!"
-        )
-
     score, level = None, None
 
     # SCORES
-    if platform in ["codechef", "codewars", "hacker-rank"]:
+    if platform in ["codechef", "codewars"]:
+        if len(prob_name_parts) < 2:
+            raise NameError(
+                f"{platform}/{prob_name} has < 2 score or difficulty level in name (--)!"
+            )
+
         score = int(prob_name_parts[0])
 
     # LEVELS
     elif platform in ["hacker-earth", "leetcode", "interviewbit", "machinehack"]:
+        if len(prob_name_parts) < 2:
+            raise NameError(
+                f"{platform}/{prob_name} has < 2 score or difficulty level in name (--)!"
+            )
+
         level = prob_name_parts[0]
 
     # SCORE & LEVEL
-    elif platform in []:
-        pass
+    elif platform in ["hacker-rank"]:
+        if len(prob_name_parts) < 3:
+            raise NameError(
+                f"{platform}/{prob_name} has < 3 score or difficulty level in name (--)!"
+            )
+
+        level = prob_name_parts[0]
+        score = int(prob_name_parts[1])
 
     else:
         raise NotImplementedError(
@@ -26,12 +37,18 @@ def get_platform_score_level(platform: str, prob_name: str):
 
     if level:
         match level:
+            # normal cases
             case "E":
                 level = "EASY"
             case "M":
                 level = "MEDIUM"
             case "H":
                 level = "HARD"
+
+            # hacker rank
+            case "A":
+                level = "ADVANCED"
+
             case _:
                 raise NotImplementedError(f"Difficulty Level `{level}` is not handled!")
 
---
+++ platforms/hacker-rank/E--10--sock-merchant/solutions/solution.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+
+def sockMerchant(n, ar):
+    freq = Counter()
+    pairs = 0
+    for e in ar:
+        freq[e] += 1
+        pairs += int(freq[e] % 2 == 0)
+    return pairs
+
+
+if __name__ == "__main__":
+    fptr = open(os.environ["OUTPUT_PATH"], "w")
+    n = int(input().strip())
+    ar = list(map(int, input().rstrip().split()))
+    result = sockMerchant(n, ar)
+    fptr.write(str(result) + "\n")
+    fptr.close()
