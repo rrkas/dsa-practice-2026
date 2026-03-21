3dd0270193c47e68b03faf7587183e249af1a4f3
Rohnak Agarwal <rrka79wal@gmail.com>
2026-03-21 19:15:13 +0530

    wip

---
+++ platforms/hacker-rank/E--15--beautiful-days-at-the-movies/solutions/solution.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'beautifulDays' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER i
+ *  2. INTEGER j
+ *  3. INTEGER k
+ */
+
+func abs(a int32) int32 {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func reverse(a int32) int32 {
+	r := int32(0)
+	for a > 0 {
+		r = 10*r + (a % 10)
+		a /= 10
+	}
+	return r
+}
+
+func beautifulDays(i int32, j int32, k int32) int32 {
+	c := int32(0)
+	for ; i <= j; i++ {
+		diff := abs(i - reverse(i))
+		if diff%k == 0 {
+			c++
+		}
+	}
+	return c
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	firstMultipleInput := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	iTemp, err := strconv.ParseInt(firstMultipleInput[0], 10, 64)
+	checkError(err)
+	i := int32(iTemp)
+
+	jTemp, err := strconv.ParseInt(firstMultipleInput[1], 10, 64)
+	checkError(err)
+	j := int32(jTemp)
+
+	kTemp, err := strconv.ParseInt(firstMultipleInput[2], 10, 64)
+	checkError(err)
+	k := int32(kTemp)
+
+	result := beautifulDays(i, j, k)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
